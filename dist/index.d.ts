5260596fa4a4cf7f5bd6a3c21ef1452d771b82cf
mr. keith <keith@hoerling.com>
2019-09-16 15:38:21 -0700

    adds LoginResponse interface
    - bump version @ 0.0.9 for release

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,10 @@ export declare type SocialParams = 'google' | 'facebook' | 'twitter' | 'linkedin
 export interface UserParams {
     email: string;
 }
+export interface LoginResponse {
+    readonly success: boolean;
+    readonly error?: string;
+}
 export interface Login {
     social: (socialParams: SocialParams) => string;
     passwordless: (userParams: UserParams) => Promise<void>;
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ export type SocialParams = 'google' | 'facebook' | 'twitter' | 'linkedin'
 export interface UserParams {
   email: string
 }
+export interface LoginResponse {
+  readonly success: boolean
+  readonly error?: string
+}
 export interface Login {
   social: (socialParams: SocialParams) => string
-  passwordless: (userParams: UserParams) => Promise<void>
+  passwordless: (userParams: UserParams) => Promise<LoginResponse>
 }
 
 export { login } from './api'
